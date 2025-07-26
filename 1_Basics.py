888bdcbc87a41f16d97a8c1dd5811496ee88b82f
bishalroy20 <bishalbondhon20@gmail.com>
2025-07-26 10:41:35 +0600

    type casting video uploaded

---
+++ 1_Basics.py
@@ -0,0 +1,33 @@
+
+# part 1: Basics
+print("copa python copa")
+print(12455)
+print("mama python sikhe gesi")
+print("85985")
+
+# part 2 : data types
+# No need to use int , float , string etc.
+age = 25
+interest_rate = 5.5
+is_single = True
+district = "Habiganj"
+
+print(age)
+print(age / interest_rate) 
+print(age + interest_rate)
+print(interest_rate)
+print(is_single)
+
+print( (age))
+print(type(district))
+print(type(interest_rate))
+print(type(is_single))
+name = "Anthony Gonsalves"
+
+print("amer nam " + "anthony gonsalves")
+
+print(f"amer nam {name}  amer district {district}  age is {age}")
+
+""" Alt shift a : multi line comment
+cntrl / : comment"""
+
---
+++ 2_Input_output_typecasting.py
@@ -0,0 +1,36 @@
+# part 3 : Take input
+
+""" print("Give me money ")
+input()
+# input('Give me money')
+
+qty = input()
+print(f"Money given : {qty} .")
+print("Money given : " , qty) """
+
+# Example of input
+
+money1 = input("Give me money 1 : ")
+money2 = input("Give me money 2 : ")
+
+print(type(money1))
+
+print(f"i got money from money1 : {money1} and money2 : {money2}")
+
+total = money1 + money2
+print(f"Total money is : {total}")
+
+# both the money are string type . so if we want to see the total as number we need to convert it to int or float
+
+# part 4 : Type casting
+# int() , float() , str()
+total = int(money1) + int(money2)
+print(f"Total money is : {total}")
+
+""" Google : 
+1. convert number to string : str
+2. convert decimal to integer : int
+3. convert string to float : float
+4. convert string to integer : int
+5. convert integer to string : str
+optional : list , tuple , hex , oct , bin"""
